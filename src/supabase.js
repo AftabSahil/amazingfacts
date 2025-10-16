6f1ff1c530c9752aa4d77dcf1e87f544a3932ccf
AftabSahil <aftabsahil81@gmail.com>
2025-10-16 19:29:44 +0530

    crerated env

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://shygktgabnyqispwsseq.supabase.co';
 
-const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNoeWdrdGdhYm55cWlzcHdzc2VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1OTQzNjYsImV4cCI6MjA3NjE3MDM2Nn0.bj8jGb62h7FTImmTK-UkMoc2qAJpLaDrBDJuSIVTi5o"
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
