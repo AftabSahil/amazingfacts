77325a07aaa494b01768e763a4a7570d0b35a9ac
Sahil Aftab <aftabsahil81@gmail.com>
2026-03-13 17:00:46 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,12 +10,13 @@ resource "aws_instance" "web_server" {
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
-#!/bin/bash
-sudo apt update -y
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo usermod -aG docker ubuntu
-EOF
+                  #!/bin/bash
+                  apt update -y
+                  apt install -y docker.io
+                  systemctl start docker
+                  systemctl enable docker
+                  usermod -aG docker ubuntu
+                  EOF
 
   tags = {
     Name = "amazingfacts-server"
