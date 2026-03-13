ff075deb8024aecefd4dad52f02d20e846f0bb8f
Sahil Aftab <aftabsahil81@gmail.com>
2026-03-13 16:34:07 +0530

    Update security-group.tf

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -1,13 +1,6 @@
 resource "aws_security_group" "web_sg" {
-
+  name   = "web-sg"
   vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 22
@@ -16,11 +9,17 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # open to internet
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
