c07e379d8cacb70f4953e97bf740855efb8a3830
Sahil Aftab <aftabsahil81@gmail.com>
2026-03-13 15:37:20 +0530

    Update subnet.tf

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,12 +1,13 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_subnet" "public_subnet" {
-
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
-
   tags = {
     Name = "public-subnet"
   }
-
 }
